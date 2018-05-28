fca55e9af95f0c36b0d0b6592d6c9fa28edd90f1
kmairye <kamille.rye@gmail.com>
2018-05-28 13:50:00 +0200

    my solution

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-"use strict";
 window.addEventListener("load", init);
 
+
+let stage, car, bullets=[];
+
 const settings = {
-    speed: 2,
-    width: 600,
+    speed: 10,
     height: 400,
+    width: 600,
     carWidth: 100,
-    bulletSpeed: 10,
+    movingLeft: false,
+    bulletspeed: 20,
     keys: {
         left: false,
         right: false,
@@ -14,18 +17,70 @@ const settings = {
         down: false
     }
 }
-let stage, car, bullets=[];
+
+
 function init(){
     stage = new createjs.Stage("stage");//the id of the canvas
-    createjs.Ticker.framerate = 60;
+    createjs.Ticker.framerate = 30;
     createjs.Ticker.addEventListener("tick", tock);
     console.log("load event happened");
-
+    
     car = new createjs.Container();
-    car.brand = "Ferrari";
-    car.direction="right";
+    car.direction = "right";
     car.x = settings.width/2-settings.carWidth/2;
-    car.y=settings.height-60;
+    car.y=settings.height-70;
+
+    //change direction on click
+    /* car.addEventListener(`click`, () => {
+        if(car.direction === "right") {
+            car.direction = "left";
+        } else {
+            car.direction = "right";
+        }
+    }); */
+
+    // on down
+    window.addEventListener(`keydown`, (e) => {
+        const keyName = e.key; 
+       
+        if (keyName === "ArrowLeft") {
+            settings.keys.left = true;
+        } else if(keyName === "ArrowUp") {
+            settings.keys.up = true;
+        } else if(keyName === "ArrowDown") {
+            settings.keys.down = true;
+        } else if(keyName === "ArrowRight") {
+            settings.keys.right = true;
+        }
+        console.log(keyName);
+    });
+
+    window.addEventListener(`keyup`, (e) => {
+        const keyName = e.key; 
+
+        if (keyName === "ArrowLeft") {
+            settings.keys.left = false;
+        } else if(keyName === "ArrowUp") {
+            settings.keys.up = false;
+        } else if(keyName === "ArrowDown") {
+            settings.keys.down = false;
+        } else if(keyName === "ArrowRight") {
+            settings.keys.right = false;
+        } 
+        if(keyName === " "){
+            shoot();
+        }
+    });
+
+function shoot() {
+    let bullet = new createjs.Shape();
+    bullet.graphics.beginFill("#a55").drawCircle(0,0,4);
+    bullet.x = car.x;
+    bullet.y = car.y;
+    bullets.push(bullet);   
+    stage.addChild(bullet);
+    console.log("shoot")
+}
     let box = new createjs.Shape();
     box.graphics.beginFill("#bada55");
     box.graphics.drawRect(0,0, settings.carWidth, 60);
@@ -43,98 +98,49 @@ function init(){
     circle2.graphics.drawCircle(0,0, 10);
     circle2.x=80;
     circle2.y=60;
+
     car.addChild(circle2);
     stage.addChild(car);
-    car.addEventListener("click", ()=>{
-        if(car.direction==="right"){
-            car.direction="left";
-        } else {
-            car.direction="right";
-        }
-    });
-    window.addEventListener("keydown", e=>{
-        console.log(e.key);
-        if(e.key === "ArrowLeft"){
-            settings.keys.left=true;
-        }
-        if(e.key === "ArrowRight"){
-            settings.keys.right=true;
-        }
-        if(e.key === "ArrowUp"){
-            settings.keys.up=true;
-        }
-        if(e.key === "ArrowDown"){
-            settings.keys.down=true;
-        }
-    })
-    window.addEventListener("keyup", e=>{
-        console.log(e.key);
-        if(e.key === "ArrowLeft"){
-            settings.keys.left=false;
-        }
-        if(e.key === "ArrowRight"){
-            settings.keys.right=false;
-        }
-        if(e.key === "ArrowUp"){
-            settings.keys.up=false;
-        }
-        if(e.key === "ArrowDown"){
-            settings.keys.down=false;
-        }
-        if(e.key === " "){
-            shoot();
-        }
-    })
-}
-function shoot(){
-    let bullet = new createjs.Shape();
-    bullet.graphics.beginFill("#fff").drawCircle(0,0,4);
-    bullet.x = car.x;
-    bullet.y=car.y;
-    bullets.push(bullet);
-    stage.addChild(bullet);
+
 }
 //github.com/jofhatkea/
 
-function moveBullets(){
-    bullets.forEach(b=>{
+
+//function for the bullets
+function moveBullets() {
+     bullets.forEach(b=>{
         b.y-=settings.bulletSpeed;
-    });
+        //console.log("move bullets")
+    })
 }
-function tock(e){//refractoring
+function tock(e){
     moveBullets();
-    if(settings.keys.left){
+    // make the arrow keys the control keys of the hero
+    if(settings.keys.left) {
         car.x-=settings.speed;
     }
-    if(settings.keys.right){
-        car.x+=settings.speed;
-    }
-    if(settings.keys.up){
+    if(settings.keys.up) {
         car.y-=settings.speed;
     }
-    if(settings.keys.down){
+    if(settings.keys.down) {
         car.y+=settings.speed;
     }
-
-    /*if(car.direction==="right"){
+    if(settings.keys.right) {
         car.x+=settings.speed;
-        if(car.x>settings.width-settings.carWidth){
-            car.direction="left";
+    }
+    // change direction when the edge of the square is reached
+    /* if(car.direction==="right"){
+        car.x+=settings.speed;
+        if(car.x>settings.width-settings.carWidth) {
+            car.direction = "left";
         }
     } else {
         car.x-=settings.speed;
-        if(car.x<=0){
-            car.direction="right";
+        if(car.x<=0) {
+            car.direction = "right";
         }
-    }*/
+    } */
     stage.update(e);
 
 }
 
-
-
-//1 a game, 
-
-//2 code journey
-
-//3 what createjs can do
