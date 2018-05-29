33ecbe208bc7caf0c438f937bea90108a09c7648
kmairye <kamille.rye@gmail.com>
2018-05-29 09:51:58 +0200

    car cannot go beyond borders

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
+"use strict";
 window.addEventListener("load", init);
 
-
-let stage, car, bullets=[];
-
 const settings = {
-    speed: 10,
-    height: 400,
+    speed: 8,
     width: 600,
+    height: 400,
     carWidth: 100,
-    movingLeft: false,
-    bulletspeed: 20,
+    bulletSpeed: 20,
     keys: {
         left: false,
         right: false,
@@ -17,18 +14,44 @@ const settings = {
         down: false
     }
 }
-
+let stage, car, bullets=[];
 
 function init(){
     stage = new createjs.Stage("stage");//the id of the canvas
-    createjs.Ticker.framerate = 30;
+    createjs.Ticker.framerate = 60;
     createjs.Ticker.addEventListener("tick", tock);
     console.log("load event happened");
     
     car = new createjs.Container();
-    car.direction = "right";
+    car.brand = "Ferrari";
+
+    car.direction="right";
     car.x = settings.width/2-settings.carWidth/2;
     car.y=settings.height-70;
+    
+    let box = new createjs.Shape();
+    box.graphics.beginFill("#bada55");
+    box.graphics.drawRect(0,0, settings.carWidth, 60);
+    car.addChild(box);
+
+    let circle = new createjs.Shape();
+    circle.graphics.beginFill("#c0ffee");
+    circle.graphics.drawCircle(0,0, 10);
+    circle.x=20;
+    circle.y=60;
+    car.addChild(circle);
+
+    let circle2 = new createjs.Shape();
+    circle2.graphics.beginFill("#c0ffee");
+    circle2.graphics.drawCircle(0,0, 10);
+    circle2.x=80;
+    circle2.y=60;
+    car.addChild(circle2);
+    stage.addChild(car);
+
+    car.addChild(circle2);
+    stage.addChild(car);
+
 
     //change direction on click
     /* car.addEventListener(`click`, () => {
@@ -70,77 +93,58 @@ function init(){
         if(keyName === " "){
             shoot();
         }
-    });
-
-function shoot() {
-    let bullet = new createjs.Shape();
-    bullet.graphics.beginFill("#a55").drawCircle(0,0,4);
-    bullet.x = car.x;
-    bullet.y = car.y;
-    bullets.push(bullet);   
-    stage.addChild(bullet);
-    console.log("shoot")
-}
-    let box = new createjs.Shape();
-    box.graphics.beginFill("#bada55");
-    box.graphics.drawRect(0,0, settings.carWidth, 60);
-    car.addChild(box);
-
-    let circle = new createjs.Shape();
-    circle.graphics.beginFill("#c0ffee");
-    circle.graphics.drawCircle(0,0, 10);
-    circle.x=20;
-    circle.y=60;
-    car.addChild(circle);
-
-    let circle2 = new createjs.Shape();
-    circle2.graphics.beginFill("#c0ffee");
-    circle2.graphics.drawCircle(0,0, 10);
-    circle2.x=80;
-    circle2.y=60;
-
-    car.addChild(circle2);
-    stage.addChild(car);
-
-}
-//github.com/jofhatkea/
-
-
-//function for the bullets
-function moveBullets() {
-     bullets.forEach(b=>{
-        b.y-=settings.bulletSpeed;
-        //console.log("move bullets")
     })
 }
-function tock(e){
-    moveBullets();
-    // make the arrow keys the control keys of the hero
-    if(settings.keys.left) {
+function shoot(){
+    let bullet = new createjs.Shape();
+    bullet.graphics.beginFill("#fff").drawCircle(0,0,4);
+    bullet.x = car.x+50;
+    bullet.y=car.y;
+    bullets.push(bullet);
+    stage.addChild(bullet);
+}
+    
+//function for the bullets
+function moveBullets(){
+    bullets.forEach(b=>{
+        b.y-=settings.bulletSpeed;
+    });
+}
+function moveHero() {
+    if(settings.keys.left){
         car.x-=settings.speed;
+        if(car.x<0) {
+            car.x = 0;
+            console.log("stop!")
+        }
     }
-    if(settings.keys.up) {
+    if(settings.keys.right){
+        car.x+=settings.speed;
+        if(car.x+settings.carWidth > settings.width){
+            car.x = settings.width-carWidth;
+
+        }
+    }
+    if(settings.keys.up){
         car.y-=settings.speed;
     }
-    if(settings.keys.down) {
+    if(settings.keys.down){
         car.y+=settings.speed;
     }
-    if(settings.keys.right) {
+}
+function tock(e){//refractoring
+    moveBullets();
+    moveHero();
+    /*if(car.direction==="right"){
         car.x+=settings.speed;
-    }
-    // change direction when the edge of the square is reached
-    /* if(car.direction==="right"){
-        car.x+=settings.speed;
-        if(car.x>settings.width-settings.carWidth) {
-            car.direction = "left";
+        if(car.x>settings.width-settings.carWidth){
+            car.direction="left";
         }
     } else {
         car.x-=settings.speed;
-        if(car.x<=0) {
-            car.direction = "right";
+        if(car.x<=0){
+            car.direction="right";
         }
-    } */
+    }*/
     stage.update(e);
-
 }
-
